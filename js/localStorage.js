3ec58f4dd422b676cc96e5e36f24b89bc0e7e92d
Baca <pawel.baca@atos.net>
2019-10-03 15:08:13 +0200

    local and http get

---
+++ js/localStorage.js
@@ -0,0 +1,9 @@
+export const localStorage = {
+    set: function(namespace, data){
+        return localStorage.setItem(namespace, JSON.stringify(data));
+    },
+    get: function(namespace){
+        var store = localStorage.getItem(namespace);
+        return (store && JSON.parse(store)) || [];
+    }
+}
